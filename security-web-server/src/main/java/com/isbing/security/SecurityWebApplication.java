15cdabd806cf7134bc22e814a8c8613ec247c763
songbing <404863881@qq.com>
2019-05-08 19:39:29 +0800

    权限服务

--- security-web-server/src/main/java/com/isbing/security/SecurityWebApplication.java
+++ security-web-server/src/main/java/com/isbing/security/SecurityWebApplication.java
@@ -1,5 +1,6 @@
 package com.isbing.security;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * Created time 2019/5/6 13:24
  */
 @SpringBootApplication(scanBasePackages = { "com.isbing" })
+@MapperScan({ "com.isbing.security", "com.isbing.other" })
 public class SecurityWebApplication {
 
 	public static void main(String[] args) {
